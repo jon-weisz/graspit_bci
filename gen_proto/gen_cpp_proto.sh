5254f514abe9ee23307d75a7b47360b880465695
jon-weisz <jweisz@cs.columbia.edu>
2014-02-20 12:37:53 -0500

    removed autogenerated files

--- gen_proto/gen_cpp_proto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SRC=${GRASPIT}/protocols
-DST=${GRASPIT}/gen_proto
-
-protoc --proto_path=${SRC} --cpp_out=${DST} ${SRC}/GraspitMessage.proto 
-protoc --proto_path=${SRC} --cpp_out=${DST} ${SRC}/Drawable.proto 
