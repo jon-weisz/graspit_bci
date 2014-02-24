1a413887d5cf46baaf204028010eab2ef61c32f6
jon-weisz <jweisz@cs.columbia.edu>
2014-02-24 11:14:59 -0500

    Moved more things in to online planner controller

--- include/BCI/onlinePlannerController.h
+++ include/BCI/onlinePlannerController.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 
+namespace bci_experiment{
+
+
 class OnLinePlanner * op;
 
 class OnlinePlannerController : public QObject
@@ -11,13 +14,21 @@ class OnlinePlannerController : public QObject
 public:
     explicit OnlinePlannerController(QObject *parent = 0);
     void analyzeApproachDir(OnLinePlanner * op);
+    void initializeDbInterface();
+    void updateObject(GraspableBody * newTarget);
 
+private:
+    db_planner::SqlDatabaseManager * mDbMgr;
 
 signals:
+    void createPlanner();
+    void targetSelected();
 
 public slots:
     void plannerTimedUpdate();
-
+    void initializeTarget(Hand * currentHand,
+                          GraspableBody * targetObject);
 };
 
+}
 #endif // ONLINEPLANNERCONTROLLER_H
--- src/BCI/onlinePlannerController.cpp
+++ src/BCI/onlinePlannerController.cpp
@@ -1,17 +1,24 @@
-#include "onlinePlannerController.h"
-#include "uiTools.h"
+#include "BCI/onlinePlannerController.h"
+#include "BCI/uiTools.h"
+#include "BCI/plannerTools.h"
+#include "BCI/worldElementTools.h"
 #include <onLinePlanner.h>
+#include "robot.h"
+#include "searchState.h"
+#include "grasp.h"
 
+namespace bci_experiment{
 
 OnlinePlannerController::OnlinePlannerController(QObject *parent) :
-    QObject(parent)
+    QObject(parent),
+    mDBMgr(NULL)
 {
 }
 
 
 void
 OnlinePlannerController::analyzeApproachDir(OnLinePlanner * op)
-{
+{    
     if(!op)
         return;
 
@@ -49,3 +56,79 @@ OnlinePlannerController::plannerTimedUpdate()
     }
     QTimer::singleShot(1000, this, SLOT(plannerTimedUpdate()));
 }
+
+
+void OnlinePlannerController::initializeDbInterface()
+{
+    if (!mDbMgr)
+    {
+      EGPlanner * planner = planner_tools::getCurrentPlanner();
+      if(!planner || !planner->getHand())
+          return;
+
+      // If it is an online planner
+      mDbMgr = new db_planner::SqlDatabaseManager("tonga.cs.columbia.edu", 5432,
+                          "postgres","roboticslab","armdb",
+                          new GraspitDBModelAllocator,
+                          new GraspitDBGraspAllocator(planner->getHand()));
+
+      planner_tools::importGraspsFromDBMgr(planner, mDbMgr);
+    }
+
+    // If this planner is an online planner, reorder the solution list
+    OnlinePlanner * op = dynamic_cast<OnLinePlanner *>(mPlanner);
+    if(op)
+        op->updateSolutionList();
+
+}
+
+
+void OnlinePlannerController::updateObject(GraspableBody * newTarget)
+{
+    EGPlanner * planner = planner_tools::getCurrentPlanner();
+    if(planner)
+    {
+        if(!planner->getHand())
+            return;
+        planner->getHand()->getGrasp()->setObjectNoUpdate(newTarget);
+    }
+}
+
+
+void OnlinePlannerController::initializeTarget(Hand * currentHand,
+                                               GraspableBody * targetObject)
+{
+    OnLinePlanner * op =
+            dynamic_cast<OnLinePlanner *>(planner_tools::getCurrentPlanner());
+
+    // Ask someone to create a planner for us
+    if(!op)
+        return;
+
+    // Set the grasps target to the new object
+    updateObject(targetObject);
+
+    // Disable collisions between any nontarget objects and the hand
+    world_element_tools::disableNontargetCollisions(currentHand, targetObject);
+    world_element_tools::disableTableObjectCollisions();
+
+    //start planner
+    op->resetPlanner();
+
+    // Download grasps from database and load them in to the planner
+    initializeDbInterface();
+
+    // Set the hand to it's highest ranked grasp
+    if(op->getListSize())
+        op->getGrasp(0)->execute(currentHand);
+
+    // Realign the hand with respect to the object, moving the hand back to its
+    // pregrasp pose
+    world_element_tools::realignHand(currentHand);
+
+    emit targetSelected();
+}
+
+
+}
+
