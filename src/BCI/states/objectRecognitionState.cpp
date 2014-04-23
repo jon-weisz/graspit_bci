2816a672033994107b1d9d924869c30613227d98
jvarley <jakevarley@gmail.com>
2014-04-23 13:30:42 -0400

    fixed camera origin request

--- src/BCI/states/objectRecognitionState.cpp
+++ src/BCI/states/objectRecognitionState.cpp
@@ -15,7 +15,7 @@ void ObjectRecognitionState::onEntry(QEvent *e)
   {
     objectRecognitionView->show();
     bciControlWindow->currentState->setText("Object Recognition State");
-
+    BCIService::getInstance()->getCameraOrigin(NULL,NULL);
     BCIService::getInstance()->runObjectRecognition(NULL,NULL);
 
     if(OnlinePlannerController::getInstance()->hasRecognizedObjects())
